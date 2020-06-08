f789007ea5077282a0b553caec3cb630b2aed7b0
Konstantin Astapov <konstantin.astapov@broadcom.com>
2020-06-08 19:08:10 +0200

    Day 1: Basic navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WidgetComponent } from './pages/widget/widget.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthGuardService } from './core/auth/auth-guard.service';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'widget',
+    component: WidgetComponent,
+    canActivate: [AuthGuardService]
+  },
+  {path: '**', redirectTo: '/widget'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { WidgetComponent } from './pages/widget/widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    WidgetComponent
   ],
   imports: [
     BrowserModule,
