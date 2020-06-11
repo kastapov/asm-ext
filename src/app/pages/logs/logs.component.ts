d7ed2b2bf80c301c194a89e01936a1ca81530fb4
Konstantin Astapov <konstantin.astapov@broadcom.com>
2020-06-11 10:45:05 +0200

    Day 4: Unsubscribe on destroy

--- src/app/pages/logs/logs.component.ts
+++ src/app/pages/logs/logs.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ILogEntry } from '../../types/ILogEntry';
 import { LogsService } from './logs.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.scss']
 })
-export class LogsComponent implements OnInit {
-
+export class LogsComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
   constructor(private logsService: LogsService) { }
 
   logs: Array<ILogEntry>;
 
   ngOnInit(): void {
-    this.logsService.subscribeForLogs().subscribe(logs => this.logs = logs);
+    this.subscription = this.logsService.getLogsObservable().subscribe(logs => this.logs = logs);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/pages/logs/logs.service.ts
+++ src/app/pages/logs/logs.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { API_BASE } from '../../../background/config';
 import { LogsRequest } from '../../types/LogsRequest';
-import { interval, Observable } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 import { startWith, switchMap } from 'rxjs/operators';
 import { ILogEntry } from '../../types/ILogEntry';
 import { DateUtil } from '../../util/date-util';
@@ -11,18 +11,18 @@ import { DateUtil } from '../../util/date-util';
   providedIn: 'root'
 })
 export class LogsService {
-  private subscription;
+  private observable$: Observable<Array<ILogEntry>>;
   constructor(private http: HttpClient) { }
 
-  public subscribeForLogs(): Observable<Array<ILogEntry>> {
-    if (!this.subscription) {
-      this.subscription = interval(60000)
+  public getLogsObservable(): Observable<Array<ILogEntry>> {
+    if (!this.observable$) {
+      this.observable$ = interval(60000)
         .pipe(
           startWith(0),
           switchMap(() => this.loadLogs())
         );
     }
-    return this.subscription;
+    return this.observable$;
   }
 
   private loadLogs(): Observable<Array<ILogEntry>> {
