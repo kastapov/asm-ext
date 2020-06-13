9a72061ec835b67f2c011859f2ed8d67c24edad5
Konstantin Astapov <konstantin.astapov@broadcom.com>
2020-06-13 12:44:04 +0200

    Day 6: Dates in logs list

--- src/app/pages/logs/log-entry/log-entry.component.ts
+++ src/app/pages/logs/log-entry/log-entry.component.ts
@@ -1,23 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { ChromeService } from '../../../core/service/chrome.service';
 import { ILog } from '../../../types/log/ILog';
+import { DateUtil } from '../../../util/date-util';
 
 @Component({
   selector: 'app-log-entry',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './log-entry.component.html',
   styleUrls: ['./log-entry.component.scss']
 })
 export class LogEntryComponent implements OnInit {
 
   @Input() logEntry: ILog;
+  @Input() previousDate: string;
 
   constructor(private chromeService: ChromeService) { }
 
   ngOnInit(): void {
   }
 
-  openLog() {
+  openLog(): void {
     this.chromeService.openTab(`reports/check/detail/${this.logEntry.monitor.id}/${this.logEntry.id}`);
   }
 
+  isToShowDate(): boolean {
+    console.log(DateUtil.ISOToDate(this.logEntry.start).getDate());
+    console.log(DateUtil.ISOToDate(this.previousDate).getDate());
+    return DateUtil.ISOToDate(this.logEntry.start).getDate() !== DateUtil.ISOToDate(this.previousDate).getDate();
+  }
 }
