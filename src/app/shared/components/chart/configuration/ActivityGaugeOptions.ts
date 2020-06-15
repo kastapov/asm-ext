1765fe053f483342858c88a0c31365cebc93c76e
Konstantin Astapov <konstantin.astapov@broadcom.com>
2020-06-15 15:05:08 +0200

    Day 8: Better gauge chart for performance

--- src/app/shared/components/chart/configuration/ActivityGaugeOptions.ts
+++ src/app/shared/components/chart/configuration/ActivityGaugeOptions.ts
@@ -6,13 +6,45 @@ export const ActivityGaugeOptions: Partial<ChartOptions> = {
   },
   plotOptions: {
     radialBar: {
+      offsetY: 0,
+      startAngle: 0,
+      endAngle: 270,
+      hollow: {
+        margin: 5,
+        size: "30%",
+        background: "transparent",
+        image: undefined
+      },
+      dataLabels: {
+        name: {
+          show: false
+        },
+        value: {
+          show: false
+        }
+      }
     }
   },
   dataLabels: {
-    enabled: true,
-    formatter: function (val, opts) {
-      return opts.w.config.series[opts.seriesIndex]
+    enabled: true
+  },
+  legend: {
+    show: true,
+    floating: true,
+    fontSize: "13px",
+    position: "left",
+    offsetX: 0,
+    offsetY: 4,
+    labels: {
+      useSeriesColors: true
     },
+    formatter: function(seriesName, opts) {
+      debugger;
+      return seriesName + ":  " + (opts.w.globals.series[opts.seriesIndex] || 0);
+    },
+    itemMargin: {
+      horizontal: 3
+    }
   },
   labels: ["Resolve", "Connect", "Processing", "Transfer"]
 }
